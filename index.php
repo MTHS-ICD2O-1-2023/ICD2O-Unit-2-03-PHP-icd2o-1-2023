0c5f0266031d6f1ba70733d5d63fbd9b218201e9
ICD2O-1 2023 <159184792+icd2o-1-2023@users.noreply.github.com>
2024-03-18 18:48:40 +0000

    init

--- index.php
+++ index.php
@@ -1,61 +1,55 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-  <meta charset="utf-8" />
-  <meta name="description" content="The XXX program, in PHP" />
-  <meta name="keywords" content="mths, ics2o" />
-  <meta name="author" content="Mr. Coxall" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="./css/style.css" />
-  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.pink-purple.min.css" />
-  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
-  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
-  <link rel="manifest" href="site.webmanifest" />
-  <title>The XXX program,in PHP</title>
-</head>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="description" content="The Hello World program, in PHP" />
+    <meta name="keywords" content="mths, ics2o" />
+    <meta name="author" content="Mr. Coxall" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.pink-purple.min.css" />
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+    <link rel="manifest" href="site.webmanifest" />
+    <title>The Hello World program,in PHP</title>
+  </head>
 
-<body>
-  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-    <header class="mdl-layout__header">
-      <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">The XXX program, in PHP</span>
-      </div>
-    </header>
-    <main class="mdl-layout__content">
-      <div class="right-image">
-        <img src="./images/notebook-animation.svg" alt="laptop image" width="250" />
-      </div>
-      <div class="page-content">Enter ...</div>
-      <br />
-      <!-- Simple Textfield for integers-->
-      <form action="#">
-        <div class="mdl-textfield mdl-js-textfield">
-          <!-- input pattern attribute -->
-          <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="length" />
-          <!-- mdl-textfield__label -->
-          <label class="mdl-textfield__label" for="demo-input">Length of ... (mm)</label>
-          <!-- class "mdl-textfield__error" -->
-          <span class="mdl-textfield__error">Input is not a number</span>
+  <body>
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+          <span class="mdl-layout-title">The Hello World program, in PHP</span>
         </div>
-      </form>
-      <br />
-      <!-- Accent-colored raised button with ripple -->
-      <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" onclick="myButtonClicked()">
-        Calculate
-      </button>
-      <br />
-      <br />
-      <div class="page-content-php">
-        <div id="answer">
-          <?php echo '<p>Hello, World!</p>'; ?>
+      </header>
+      <main class="mdl-layout__content">
+        <div class="right-image">
+          <img src="./images/notebook-animation.svg" alt="laptop image" width="250" />
         </div>
-      </div>
-    </main>
-  </div>
-</body>
+        <div class="page-content-php">
+          <form action="answer.php" method="POST">
+            <p>Name</p>
+            <div class="mdl-textfield mdl-js-textfield">
+              <input class="mdl-textfield__input" type="text" name="name">
+              <label class="mdl-textfield__label" for="name-input">Name here ...</label>
+            </div>
+            <br />
+            <p>Age</p>
+            <div class="mdl-textfield mdl-js-textfield">
+              <input class="mdl-textfield__input" type=" text" pattern="-?[0-9]*(\.[0-9]+)?" name="age">
+              <label class="mdl-textfield__label" for="age-input">Age here ...</label>
+              <span class="mdl-textfield__error">Input is not a number!</span>
+            </div>
+            <br />
+            <!-- Accent-colored raised button with ripple -->
+            <input class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit">
+          </form>
+        </div>
+      </main>
+    </div>
+  </body>
 
 </html>
